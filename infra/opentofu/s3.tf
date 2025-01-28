99061d04ed0aa6abaacbbd08a391c9eaff2fbec0
Matheus Rosmaninho <matheusrosmano@gmail.com>
2025-01-27 22:05:36 -0300

    fix: Ajuste pra public read

--- infra/opentofu/s3.tf
+++ infra/opentofu/s3.tf
@@ -27,6 +27,38 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "infra_site_bucket
   ]
 }
 
+resource "aws_s3_bucket_ownership_controls" "infra_site_bucket_ownership" {
+  bucket = aws_s3_bucket.infra_site_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+
+  depends_on = [
+    aws_s3_bucket.infra_site_bucket
+   ]
+}
+
+resource "aws_s3_bucket_public_access_block" "infra_site_bucket_public_access" {
+  bucket = aws_s3_bucket.infra_site_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+
+  depends_on = [ aws_s3_bucket.infra_site_bucket ]
+}
+
+resource "aws_s3_bucket_acl" "infra_site_bucket" {
+  bucket = aws_s3_bucket.infra_site_bucket.id
+  acl    = "public-read"
+
+  depends_on = [
+    aws_s3_bucket_ownership_controls.infra_site_bucket,
+    aws_s3_bucket_public_access_block.infra_site_bucket,
+  ]
+}
+
 resource "aws_s3_bucket_website_configuration" "infra_site_bucket_website" {
   bucket = aws_s3_bucket.infra_site_bucket.id
 
